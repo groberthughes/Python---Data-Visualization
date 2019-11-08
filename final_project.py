fb250e9f939a63ff2d4a5c76a9cc806df7420c8a
George Hughes <georgerhughes@email.arizona.edu>
2019-11-08 14:39:09 -0700

    fixed error in final_project.csv

--- final_project.py
+++ final_project.py
@@ -3,7 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
-	df = pd.read_csv("steam.csv")
+	df = pd.read_csv("C:\\Users\\ogree\\repos\\ista-131-final\\steam.csv")
 
 	# df.drop(df[df.median_playtime > 500].index)
 	df.plot(kind='scatter', x='price', y='average_playtime')
