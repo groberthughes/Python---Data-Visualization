b3b3c01b49ff7766398294f8d2bf100b5ccdebaa
George Hughes <georgerhughes@email.arizona.edu>
2019-11-08 14:07:20 -0700

    adding final_project.py

---
+++ final_project.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def main():
+	df = pd.read_csv("steam.csv", nrows=50)
+
+	df.drop(df[df.median_playtime > 500].index)
+	df.plot(kind='scatter', x='price', y='average_playtime')
+	plt.show()
+	
+
+main()
+
+def ownersToInteger():
+	ownersMinMax = df.iloc[x]["owners"].split("-")
+	ownersMinMax[0] = int(ownersMinMax[0])
+	ownersMinMax[1] = int(ownersMinMax[1])
+	ownersMean = (ownersMinMax[0] +  ownersMinMax[1]) / 2
+	print(ownersMean)
+	df.ix[x, 'owners'] = ownersMean
+	print(df.iloc[1])
